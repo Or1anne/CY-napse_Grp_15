83648ce8772ebb1e4a6c69ea15b3781b738ee1eb
MathisLanceRichardot <153006795+MathisLanceRichardot@users.noreply.github.com>
2025-05-10 23:51:44 +0200

    Private + getteur/setteur
    ajout des fonction pour le tremaux

--- src/main/java/com/example/nouveau/Case.java
+++ src/main/java/com/example/nouveau/Case.java
@@ -1,24 +1,39 @@
 package com.example.nouveau;
 
 public class Case {
+    private int x, y;
     private int id;
     private static int number;
-    public boolean North;
-    public boolean South;
-    public boolean East;
-    public boolean West;
-    public boolean visited;
+    private boolean North = true, South = true, East = true, West = true;
+    private boolean visited = false;
+    private int count = 0;
 
-    public Case(int x, int y){
-        id = number++;
-        North = true;
-        South = true;
-        East = true;
-        West = true;
-        visited = false;
+    public Case(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.id = number++;
     }
 
-    public int getID(){
-        return this.id;
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public int getID() { return id; }
+    public boolean getNorth() { return North; }
+    public boolean getSouth() { return South; }
+    public boolean getWest() { return West; }
+    public boolean getEast() { return East; }
+    public boolean getVisited() { return visited; }
+    public int getCount() { return count; }
+
+    public void setNorth(boolean North) { this.North = North; }
+    public void setSouth(boolean South) { this.South = South; }
+    public void setWest(boolean West)   { this.West = West; }
+    public void setEast(boolean East)   { this.East = East; }
+    public void setVisited(boolean visited) { this.visited = visited; }
+
+    public void incrementCount() { this.count++; }
+
+    @Override
+    public String toString() {
+        return "Case{x=" + x + ", y=" + y + ", count=" + count + "}";
     }
 }
