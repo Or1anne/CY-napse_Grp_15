0331daff826edb6ef552bb464703d1f543e94567
MathisLanceRichardot <lancericha@cy-tech.fr>
2025-05-12 05:31:14 +0200

    Définitions des directions

--- src/main/java/com/example/nouveau/Direction.java
+++ src/main/java/com/example/nouveau/Direction.java
@@ -1,16 +1,19 @@
 package com.example.nouveau;
 
 public enum Direction{
+
     NORTH, EAST, SOUTH, WEST;
 
+    //Pour tourner à gauche
     public Direction turnLeft() {
         return values()[(this.ordinal() + 3) % 4];
     }
-
+    //Pour tourner à droite
     public Direction turnRight() {
         return values()[(this.ordinal() + 1) % 4];
     }
 
+    //Avancer droit
     public Direction opposite() {
         return values()[(this.ordinal() + 2) % 4];
     }
